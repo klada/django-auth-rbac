0051fe77ce02ed7c7552bb6bc86c66eedd17852d
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2013-07-19 09:26:25 +0200

    Remove __init__.py hack

--- rbac/__init__.py
+++ rbac/__init__.py
@@ -1,22 +1,4 @@
-import sys
 from django.conf import settings
-
-# Fall back to 'rbac.RbacUser' if
-# settings.AUTH_USER_MODEL is not defined.
-if settings.AUTH_USER_MODEL.lower() == 'auth.user' \
-   or settings.AUTH_USER_MODEL == '':
-    settings.AUTH_USER_MODEL = 'rbac.rbacuser'
-
-if 'test' in sys.argv:
-    settings.RBAC_SKIP_TESTS = False
-    if settings.AUTH_USER_MODEL.lower() != 'rbac.rbacuser':
-        if 'rbac' in sys.argv:
-            print "NOTICE: Using RBAC user model for testing..."
-            settings.AUTH_USER_MODEL = 'rbac.rbacuser'
-        else:
-            print "WARNING: Skipping RBAC tests."
-            settings.RBAC_SKIP_TESTS = True
 
 if settings.AUTH_USER_MODEL.lower() == 'rbac.rbacuser':
     from rbac.users import RbacUser
-

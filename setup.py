678f757213a3e2809cda565384cd8f0bbe057f35
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2016-01-06 16:59:57 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django-auth-rbac',
+    version='0.1',
+    description='An attempt of implementing role-based access control (ANSI/INCITS 359-2004) for Django',
+    author='Daniel Klaffenbach',
+    url='https://github.com/klada/django-auth-rbac',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data = {
+        'rbac': [
+            'static/rbac/css/*',
+            'locale/de/LC_MESSAGES/*',
+            'templates/rbac/*'
+        ]
+    },
+    install_requires=[
+        'django>=1.7',
+    ],
+)

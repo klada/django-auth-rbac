c7e02da07bc3f6e98c759b4ebcb89d80c25e278e
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2016-01-06 16:48:42 +0100

    tests: Add settings for tests and runner script

---
+++ tests/test_settings.py
@@ -0,0 +1,55 @@
+# Settings for testing environment
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7t!#2up(&xk*rb3tpz842o-g3)2npq-$_sy!@cxwhmc0i8k4f!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rbac',
+    'tests',
+)
+
+AUTH_USER_MODEL = 'rbac.RbacUser'
+
+AUTHENTICATION_BACKENDS = [
+    'rbac.backends.RbacUserBackend'
+]
+
+ROOT_URLCONF = 'tests.urls'
+
+WSGI_APPLICATION = 'tests.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'

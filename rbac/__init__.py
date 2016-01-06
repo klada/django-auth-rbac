4be209366e5f4b05c0b33763a1a90bc773a1796d
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2016-01-06 15:19:51 +0100

    Move RbacUser to models.py

--- rbac/__init__.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-if settings.AUTH_USER_MODEL.lower() == 'rbac.rbacuser':
-    from rbac.users import RbacUser
--- rbac/models.py
+++ rbac/models.py
@@ -8,6 +8,7 @@ from django.db.models import Q
 from django.dispatch import receiver
 from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.models import ContentType
 from django.utils.html import format_html
 from django.utils.timezone import utc
@@ -435,6 +436,126 @@ class RbacUserAssignment(AbstractBaseModel):
         verbose_name_plural = _("RBAC user assignments")
 
 
+class AbstractRbacUser(models.Model):
+    """
+    This class can be used as a base class for defining custom User classes
+    whenever 'django.contrib.auth.models.User' cannot be used for whatever
+    reasons.
+
+    It does not provide any properties at all - only a minimum set of methods
+    which are required for RBAC operations. It is up to you to define the
+    properties which are needed to work with your application or with Django's
+    build-in admin.
+    """
+    __rbac_backend = None
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        abstract = True
+
+    def is_anonymous(self):
+        """
+        Always returns False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
+
+    def get_all_roles(self):
+        """
+        Returns a list of roles which are assigned to this user. This list will
+        be used for providing role choices in RbacSessions, for example.
+
+        By default we only query the RbacUserAssignment for roles. If you have
+        any other sources for roles you can override this method. You just need
+        to make sure that this method returns a QuerySet!
+
+        @rtype: QuerySet
+        """
+        from rbac.models import RbacRole
+        return RbacRole.objects.filter(rbacuserassignment__user=self)
+
+    def get_all_permissions(self, obj=None):
+        if not self.__rbac_backend:
+            from rbac.backends import RbacUserBackend
+            self.__rbac_backend = RbacUserBackend()
+        return self.__rbac_backend.get_all_permissions(self, obj)
+
+    def has_perm(self, perm, obj=None):
+        """
+        Returns True if the user has the specified permission. This method
+        only uses the RbacUserBackend for checking permissions.
+        """
+        if not self.__rbac_backend:
+            from rbac.backends import RbacUserBackend
+            self.__rbac_backend = RbacUserBackend()
+        return self.__rbac_backend.has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        """
+        Returns True if the user has each of the specified permissions. If
+        object is passed, it checks if the user has all required perms for this
+        object.
+        """
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+
+    def has_module_perms(self, app_label):
+        """
+        Returns True if the user has any permission in the specified app.
+        """
+        if not self.__rbac_backend:
+            from rbac.backends import RbacUserBackend
+            self.__rbac_backend = RbacUserBackend()
+        return self.__rbac_backend.has_module_perms(self, app_label)
+
+
+class RbacUser(AbstractUser):
+    """
+    Adds extra RBAC functionality to Django's built-in User class.
+    
+    All RBAC-models will use this model when using 'django.contrib.auth'.
+    """
+    groups = None
+    user_permissions = None
+    __rbac_backend = None
+    
+    class Meta:
+        app_label = 'rbac'
+        db_table = 'auth_rbac_user'
+        swappable = 'AUTH_USER_MODEL'
+
+    def get_all_roles(self):
+        """
+        Returns a list of roles which are assigned to this user. This list will
+        be used for providing role choices in RbacSessions, for example.
+
+        By default we only query the RbacUserAssignment for roles. If you have
+        any other sources for roles you can override this method. You just need
+        to make sure that this method returns a QuerySet!
+
+        @rtype: QuerySet
+        """
+        from rbac.models import RbacRole
+        return RbacRole.objects.filter(rbacuserassignment__user=self)
+
+
+    def get_all_permissions(self, obj=None):
+        if not self.__rbac_backend:
+            from rbac.backends import RbacUserBackend
+            self.__rbac_backend = RbacUserBackend()
+        return self.__rbac_backend.get_all_permissions(self)
+
+
 def _rbac_check_ssd_userassignment(ssd_roles_set, ssd_cardinality):
     """
     Checks if any of the current RbacUserAssignments would violate a SSD set
@@ -775,4 +896,6 @@ def _rbac_userassignment_delete(sender, instance, **kwargs):
     RbacUserAssignment.
     """
     RbacSession.objects.filter(user=instance.user).delete()
+
+
 

2b89e4cd78892b8ee40055de548c15585d479c51
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2017-07-05 16:35:43 +0200

    RbacRole: Add support for natural keys

--- rbac/models.py
+++ rbac/models.py
@@ -79,6 +79,14 @@ class RbacPermission(AbstractBaseModel):
     natural_key.dependencies = ['contenttypes.contenttype']
 
 
+class RbacRoleManager(models.Manager):
+    """
+    Custom manager with support for natural key lookup.
+    """
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
 class RbacRole(AbstractBaseModel):
     """
     A role that can be assigned to users and other roles.
@@ -89,6 +97,8 @@ class RbacRole(AbstractBaseModel):
     children =  models.ManyToManyField( 'self', symmetrical=False, blank=True)
     permissions = models.ManyToManyField(RbacPermission, blank=True)
     children_all = models.ManyToManyField( 'self', symmetrical=False, blank=True, editable=False, through="RbacRoleProfile", related_name="parents_all")
+
+    objects = RbacRoleManager()
 
     def __unicode__(self):
         if self.displayName:
@@ -167,6 +177,9 @@ class RbacRole(AbstractBaseModel):
         @rtype: QuerySet
         """
         return RbacRole.objects.filter(children=self)
+
+    def natural_key(self):
+        return (self.name, )
  
 
 class RbacRoleProfile(AbstractBaseModel):

a495324e14313e8252d29f1a051e7306b2019c39
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2017-08-30 08:01:08 +0200

    management.commands: Drop NoArgsCommand

--- rbac/management/commands/clearrbacsessions.py
+++ rbac/management/commands/clearrbacsessions.py
@@ -1,9 +1,11 @@
-from django.core.management.base import NoArgsCommand
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.management.base import BaseCommand
 from rbac.models import RbacSession
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Can be run as a cronjob or directly to remove expired RBAC sessions."
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         RbacSession.clear_expired()

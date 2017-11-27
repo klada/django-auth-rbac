ccbb3b6c12892863cbc3f3bf43e447fd6799587b
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2017-11-27 16:19:22 +0100

    backends: Do not duplicate Django code
    This reorganizes the class hierarchy of the available RBAC backends so that
    they can simply inherit all relevant functions from Django's built-in
    auth backend classes.

--- rbac/backends.py
+++ rbac/backends.py
@@ -1,16 +1,19 @@
 import inspect
 import itertools
 import logging
-from django.contrib.auth.models import User
-from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import ModelBackend, RemoteUserBackend
 from django.core.exceptions import ObjectDoesNotExist
 from rbac import models
 from rbac.models import RbacPermission, RbacPermissionProfile
 
+
 logger = logging.getLogger(__name__)
 
-class RbacUserBackend(ModelBackend):
+
+class RbacUserBackendMixin(object):
     """
+    Mixin class which adds the magic required for RBAC permission lookups to other user backends.
+
     Limitations:
         - Does not support group permissions
     """
@@ -32,7 +35,6 @@ class RbacUserBackend(ModelBackend):
             logger.info("Using RBAC backend session for user %s." %user_obj.id)
             return rbac_session
     
-    
     def has_module_perms(self, user_obj, app_label):
         if user_obj.is_anonymous:
             return False
@@ -51,7 +53,6 @@ class RbacUserBackend(ModelBackend):
             logger.debug('has_module_perms(): Using app-label permission cache for user %s' %user_obj.pk)
         
         return app_label in user_obj._rbac_module_permission_cache
-
 
     def has_perm(self, user_obj, perm, obj=None):
         if user_obj.is_anonymous:
@@ -91,7 +92,6 @@ class RbacUserBackend(ModelBackend):
                 
                 return obj._has_perm(user_obj, perm)
         return False
-
     
     def _get_all_object_permissions(self, user_obj, obj):
         """
@@ -119,7 +119,6 @@ class RbacUserBackend(ModelBackend):
                  rbacpermissionprofile__role__in=session.active_roles.all()
                 ).values_list('name', flat=True)
         return set(itertools.imap(lambda x: '%s.%s_%s' %(app_label, x, model), perms))
- 
 
     def get_all_permissions(self, user_obj, obj=None):
         """
@@ -156,62 +155,15 @@ class RbacUserBackend(ModelBackend):
         return user_obj._rbacperm_cache
         
 
-class RbacRemoteUserBackend(RbacUserBackend):
+class RbacUserBackend(RbacUserBackendMixin, ModelBackend):
     """
-    This backend is to be used in conjunction with the ``RemoteUserMiddleware``
-    found in the middleware module of django.contrib.auth, and is used when the
-    server is handling authentication outside of Django.
-
-    By default, the ``authenticate`` method creates ``User`` objects for
-    usernames that don't already exist in the database.  Subclasses can disable
-    this behavior by setting the ``create_unknown_user`` attribute to
-    ``False``.
+    A version of `django.contrib.auth.backends.ModelBackend` with support for RBAC permission lookups.
     """
+    pass
 
-    # Create a User object if not already in the database?
-    create_unknown_user = False
 
-    def authenticate(self, remote_user):
-        """
-        The username passed as ``remote_user`` is considered trusted.  This
-        method simply returns the ``User`` object with the given username,
-        creating a new ``User`` object if ``create_unknown_user`` is ``True``.
-
-        Returns None if ``create_unknown_user`` is ``False`` and a ``User``
-        object with the given username is not found in the database.
-        """
-        if not remote_user:
-            return
-        user = None
-        username = self.clean_username(remote_user)
-
-        # Note that this could be accomplished in one try-except clause, but
-        # instead we use get_or_create when creating unknown users since it has
-        # built-in safeguards for multiple threads.
-        if self.create_unknown_user:
-            user, created = User.objects.get_or_create(username=username)
-            if created:
-                user = self.configure_user(user)
-        else:
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                pass
-        return user
-
-    def clean_username(self, username):
-        """
-        Performs any cleaning on the "username" prior to using it to get or
-        create the user object.  Returns the cleaned username.
-
-        By default, returns the username unchanged.
-        """
-        return username
-
-    def configure_user(self, user):
-        """
-        Configures a user after creation and returns the updated user.
-
-        By default, returns the user unmodified.
-        """
-        return user
+class RbacRemoteUserBackend(RbacUserBackendMixin, RemoteUserBackend):
+    """
+    A version of `django.contrib.auth.backends.RemoteUserBackend` with support for RBAC permission lookups.
+    """
+    pass
